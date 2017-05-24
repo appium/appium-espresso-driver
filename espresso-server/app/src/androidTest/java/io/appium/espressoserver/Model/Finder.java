f85b4fbcba5ddb69c492e2993b437d231f242973
dan@saucelabs.com <dan@saucelabs.com>
2017-05-24 11:50:57 -0700

    Renamed android-server-app to espresso-server

---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/Model/Finder.java
@@ -0,0 +1,12 @@
+package io.appium.espressoserver.Model;
+
+import static android.support.test.espresso.Espresso.onView;
+
+/**
+ * Created by danielgraham on 5/24/17.
+ */
+
+public class Finder {
+    public Finder () {
+    }
+}
---
+++ espresso-server/app/src/main/java/io/appium/espressoserver/Model/Finder.java
@@ -0,0 +1,8 @@
+package io.appium.espressoserver.Model;
+
+/**
+ * Created by danielgraham on 5/24/17.
+ */
+
+public class Finder {
+}
