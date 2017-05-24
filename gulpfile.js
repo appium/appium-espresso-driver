7dbb0e2abcc88b554495df27d7851b3e65f57fd1
Jonathan Lipps <jlipps@gmail.com>
2017-05-24 12:17:43 -0700

    get basic js framework for espresso runner going

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+"use strict";
+
+var gulp = require('gulp'),
+    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+
+boilerplate({
+  build: 'appium-espresso-driver',
+  jscs: false,
+  testTimeout: 120000,
+  e2eTest: { android: true }
+});
