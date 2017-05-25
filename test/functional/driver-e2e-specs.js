bb04f99af180b9a2d0fbc8bb265c0d65b2c3090f
Isaac Murchie <isaac@saucelabs.com>
2017-05-25 11:44:25 -0700

    Address comments

--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -1,21 +1,13 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import wd from 'wd';
-import sampleApps from 'sample-apps';
 import { HOST, PORT, MOCHA_TIMEOUT } from './helpers/session';
+import { APIDEMO_CAPS } from './desired';
 import { startServer } from '../..';
 
 
 chai.should();
 chai.use(chaiAsPromised);
-
-let defaultCaps = {
-  androidInstallTimeout: 90000,
-  app: sampleApps('ApiDemos-debug'),
-  deviceName: 'Android',
-  platformName: 'Android',
-  forceEspressoRebuild: true,
-};
 
 describe('createSession', function () {
   this.timeout(MOCHA_TIMEOUT);
@@ -35,8 +27,8 @@ describe('createSession', function () {
     } catch (ign) {}
   });
   it('should start android session focusing on default pkg and act', async () => {
-    let status = await driver.init(defaultCaps);
+    let status = await driver.init(APIDEMO_CAPS);
 
-    status[1].app.should.eql(defaultCaps.app);
+    status[1].app.should.eql(APIDEMO_CAPS.app);
   });
 });
