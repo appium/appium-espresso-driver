858bdc4c637589547805f8702fa235af886a599d
dan@saucelabs.com <dan@saucelabs.com>
2017-06-16 10:51:40 -0700

    PR fix

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/exceptions/StaleElementException.java
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/exceptions/StaleElementException.java
@@ -2,6 +2,6 @@ package io.appium.espressoserver.lib.handlers.exceptions;
 
 public class StaleElementException extends AppiumException {
     public StaleElementException(String elementId) {
-        super(String.format("Element %s no longer exists", elementId));
+        super(String.format("The cached element %s no longer exists in the Android View hierarchy. Try to find it using a locator.", elementId));
     }
 }
