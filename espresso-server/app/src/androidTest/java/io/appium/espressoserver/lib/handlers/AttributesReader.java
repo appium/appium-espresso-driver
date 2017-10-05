2a1dcfc68d2be1d9393e73d1daacf12acc4cf735
Mykola Mokhnach <nick+1@wire.com>
2017-10-05 21:28:26 +0200

    Add support for /session/:sessionId/element/:elementId/attribute/:name endpoint (#62)
    * Update project dependencies
    
    * Implement /attribute/ route
    
    * Remove unneeded code duplication
    
    * Simplify expressions
    
    * Improve the error message

---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/AttributesReader.java
@@ -0,0 +1,86 @@
+package io.appium.espressoserver.lib.handlers;
+
+import android.support.test.espresso.ViewInteraction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import io.appium.espressoserver.lib.handlers.exceptions.AppiumException;
+import io.appium.espressoserver.lib.handlers.exceptions.NotYetImplementedException;
+import io.appium.espressoserver.lib.model.AppiumParams;
+import io.appium.espressoserver.lib.model.Element;
+import io.appium.espressoserver.lib.model.ViewAttributesEnum;
+import io.appium.espressoserver.lib.model.ViewElement;
+import io.appium.espressoserver.lib.viewaction.ViewFinder;
+import io.appium.espressoserver.lib.viewaction.ViewTextGetter;
+
+public class AttributesReader implements RequestHandler<AppiumParams, String> {
+
+    @Override
+    @Nullable
+    public String handle(AppiumParams params) throws AppiumException {
+        final String attributeName = params.getUriParameterValue("name");
+        if (attributeName == null || attributeName.trim().isEmpty()) {
+            throw new AppiumException("Attribute name cannot be null or empty");
+        }
+        ViewAttributesEnum dstAttribute = null;
+        for (ViewAttributesEnum attribute : ViewAttributesEnum.values()) {
+            if (attributeName.equalsIgnoreCase(attribute.toString())) {
+                dstAttribute = attribute;
+                break;
+            }
+        }
+        if (dstAttribute == null) {
+            final List<String> supportedAttributeNames = new ArrayList<>();
+            for (ViewAttributesEnum attribute : ViewAttributesEnum.values()) {
+                supportedAttributeNames.add(attribute.toString());
+            }
+            throw new AppiumException(
+                    String.format("Attribute name should be one of %s. '%s' is given instead",
+                            supportedAttributeNames, attributeName));
+        }
+        final ViewInteraction viewInteraction = Element.getById(params.getElementId());
+        if (dstAttribute == ViewAttributesEnum.TEXT) {
+            return new ViewTextGetter().get(viewInteraction).toString();
+        }
+        final ViewElement viewElement = new ViewElement(new ViewFinder().getView(viewInteraction));
+        switch (dstAttribute) {
+            case CONTENT_DESC:
+                return viewElement.getContentDescription().toString();
+            case CLASS:
+                return viewElement.getClassName();
+            case CHECKABLE:
+                return Boolean.toString(viewElement.isCheckable());
+            case CHECKED:
+                return Boolean.toString(viewElement.isChecked());
+            case CLICKABLE:
+                return Boolean.toString(viewElement.isClickable());
+            case ENABLED:
+                return Boolean.toString(viewElement.isEnabled());
+            case FOCUSABLE:
+                return Boolean.toString(viewElement.isFocusable());
+            case FOCUSED:
+                return Boolean.toString(viewElement.isFocused());
+            case SCROLLABLE:
+                return Boolean.toString(viewElement.isScrollable());
+            case LONG_CLICKABLE:
+                return Boolean.toString(viewElement.isLongClickable());
+            case PASSWORD:
+                return Boolean.toString(viewElement.isPassword());
+            case SELECTED:
+                return Boolean.toString(viewElement.isSelected());
+            case BOUNDS:
+                return viewElement.getBounds().toShortString();
+            case RESOURCE_ID:
+                return Integer.toString(viewElement.getResourceId());
+            case INDEX:
+                return Integer.toString(viewElement.getIndex());
+            // case PACKAGE:
+            // case INSTANCE:
+            default:
+                throw new NotYetImplementedException();
+        }
+    }
+}
--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/model/AppiumParams.java
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/model/AppiumParams.java
@@ -1,25 +1,42 @@
 package io.appium.espressoserver.lib.model;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
 @SuppressWarnings("unused")
 public class AppiumParams {
+    private static final String SESSION_ID_PARAM_NAME = "sessionId";
+    private static final String ELEMENT_ID_PARAM_NAME = "elementId";
+    private final Map<String, String> uriParams = new HashMap<>();
 
-    private String sessionId;
-    private String elementId;
-
+    @Nullable
     public String getSessionId() {
-        return sessionId;
+        return getUriParameterValue(SESSION_ID_PARAM_NAME);
     }
 
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
+    @Nullable
     public String getElementId() {
-        return elementId;
+        return getUriParameterValue(ELEMENT_ID_PARAM_NAME);
     }
 
     public void setElementId(String elementId) {
-        this.elementId = elementId;
+        setUriParameterValue(ELEMENT_ID_PARAM_NAME, elementId);
+    }
+
+    public void initUriMapping(Map<String, String> params) {
+        uriParams.clear();
+        uriParams.putAll(params);
+    }
+
+    @Nullable
+    public String getUriParameterValue(String name) {
+        return uriParams.get(name);
+    }
+
+    private void setUriParameterValue(String name, String value) {
+        uriParams.put(name, value);
     }
 }
