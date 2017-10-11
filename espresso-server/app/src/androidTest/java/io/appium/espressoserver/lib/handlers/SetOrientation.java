0835eac14e696e3e8cc411ab5eec79b85e21d175
Mykola Mokhnach <nick+1@wire.com>
2017-10-11 20:44:58 +0200

    Implement endpoints for getting and setting display orientation (#65)
    * Implement setting orientation
    
    * Implement orientation getting
    
    * Remove unnecessary condition
    
    * Make Codacy happy
    
    * Make orientation final
    
    * Avoid changing the argument

---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/SetOrientation.java
@@ -0,0 +1,39 @@
+package io.appium.espressoserver.lib.handlers;
+
+import android.support.test.espresso.ViewInteraction;
+
+import java.util.Arrays;
+
+import javax.annotation.Nullable;
+
+import io.appium.espressoserver.lib.handlers.exceptions.AppiumException;
+import io.appium.espressoserver.lib.model.Element;
+import io.appium.espressoserver.lib.model.OrientationParams;
+import io.appium.espressoserver.lib.viewaction.OrientationChange;
+
+public class SetOrientation implements RequestHandler<OrientationParams, Void> {
+
+    @Override
+    @Nullable
+    public Void handle(OrientationParams params) throws AppiumException {
+        final ViewInteraction viewInteraction = Element.getById(params.getElementId());
+        final String orientation = params.getOrientation();
+        if (orientation == null ||
+                !Arrays.asList(new String[] {"LANDSCAPE", "PORTRAIT"}).
+                        contains(orientation.toUpperCase())) {
+            throw new AppiumException(String.format("Screen orientation value to '%s'",
+                    orientation));
+        }
+        try {
+            if (orientation.equalsIgnoreCase("LANDSCAPE")) {
+                viewInteraction.perform(OrientationChange.orientationLandscape());
+            } else {
+                viewInteraction.perform(OrientationChange.orientationPortrait());
+            }
+        } catch (Exception e) {
+            throw new AppiumException(String.format("Cannot change screen orientation to '%s'",
+                    orientation), e);
+        }
+        return null;
+    }
+}
---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/viewaction/OrientationChange.java
@@ -0,0 +1,44 @@
+package io.appium.espressoserver.lib.viewaction;
+
+import android.content.pm.ActivityInfo;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+import io.appium.espressoserver.lib.helpers.ActivityFinder;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+
+public class OrientationChange implements ViewAction {
+    private final int orientation;
+
+    private OrientationChange(int orientation) {
+        this.orientation = orientation;
+    }
+
+    public static ViewAction orientationLandscape() {
+        return new OrientationChange(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    public static ViewAction orientationPortrait() {
+        return new OrientationChange(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    @Override
+    public Matcher<View> getConstraints() {
+        return isRoot();
+    }
+
+    @Override
+    public String getDescription() {
+        return "change orientation to " + orientation;
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        uiController.loopMainThreadUntilIdle();
+        ActivityFinder.extractFrom(view).setRequestedOrientation(orientation);
+    }
+}
