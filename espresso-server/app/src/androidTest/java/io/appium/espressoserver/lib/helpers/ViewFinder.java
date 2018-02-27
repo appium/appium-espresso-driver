0be101776e3517f3cd1bc999d04d3636896e1665
Mykola Mokhnach <nick+1@wire.com>
2018-02-27 20:25:15 +0100

    Implement subelements lookup (#90)

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/ViewFinder.java
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/ViewFinder.java
@@ -37,11 +37,13 @@ import io.appium.espressoserver.lib.handlers.exceptions.XPathLookupException;
 import io.appium.espressoserver.lib.model.Strategy;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static io.appium.espressoserver.lib.viewmatcher.WithXPath.withXPath;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.endsWith;
 
 /**
@@ -49,8 +51,17 @@ import static org.hamcrest.Matchers.endsWith;
  */
 public class ViewFinder {
 
+    @Nullable
+    public static ViewInteraction findBy(Strategy strategy, String selector)
+            throws InvalidStrategyException, XPathLookupException {
+        return findBy(null, strategy, selector);
+    }
+
     /**
      * Find one instance of an element that matches the locator criteria
+     *
+     * @param root     Parent view instance or null if the search is executed against the
+     *                 full hierarchy
      * @param strategy Locator strategy (xpath, class name, etc...)
      * @param selector Selector string
      * @return
@@ -58,28 +69,42 @@ public class ViewFinder {
      * @throws XPathLookupException
      */
     @Nullable
-    public static ViewInteraction findBy(Strategy strategy, String selector) throws InvalidStrategyException, XPathLookupException {
-        List<ViewInteraction> viewInteractions = findAllBy(strategy, selector, true);
+    public static ViewInteraction findBy(
+            @Nullable View root, Strategy strategy, String selector)
+            throws InvalidStrategyException, XPathLookupException {
+        List<ViewInteraction> viewInteractions = findAllBy(root, strategy, selector, true);
         if (viewInteractions.isEmpty()) {
             return null;
         }
         return viewInteractions.get(0);
     }
 
+    @Nullable
+    public static List<ViewInteraction> findAllBy(Strategy strategy, String selector)
+            throws InvalidStrategyException, XPathLookupException {
+        return findAllBy(null, strategy, selector, false);
+    }
+
     /**
      * Find all instances of an element that matches the locator criteria
+     *
+     * @param root     Parent view instance or null if the search is executed against the
+     *                 full hierarchy
      * @param strategy Locator strategy (xpath, class name, etc...)
      * @param selector Selector string
      * @return
      * @throws InvalidStrategyException
      * @throws XPathLookupException
      */
-    public static List<ViewInteraction> findAllBy(Strategy strategy, String selector) throws InvalidStrategyException, XPathLookupException {
-        return findAllBy(strategy, selector, false);
+    public static List<ViewInteraction> findAllBy(@Nullable View root,
+                                                  Strategy strategy, String selector)
+            throws InvalidStrategyException, XPathLookupException {
+        return findAllBy(root, strategy, selector, false);
     }
 
     ///Find By different strategies
-    private static List<ViewInteraction> findAllBy(Strategy strategy, String selector, boolean findOne)
+    private static List<ViewInteraction> findAllBy(@Nullable View root,
+                                                   Strategy strategy, String selector, boolean findOne)
             throws InvalidStrategyException, XPathLookupException {
         List<ViewInteraction> matcher;
 
@@ -91,28 +116,28 @@ public class ViewFinder {
                 int id = context.getResources().getIdentifier(selector, "Id",
                         InstrumentationRegistry.getTargetContext().getPackageName());
 
-                matcher = getViewInteractions(withId(id), findOne);
+                matcher = getViewInteractions(root, withId(id), findOne);
                 break;
             case CLASS_NAME:
                 // with class name
                 // TODO: improve this finder with instanceOf
-                matcher = getViewInteractions(withClassName(endsWith(selector)), findOne);
+                matcher = getViewInteractions(root, withClassName(endsWith(selector)), findOne);
                 break;
             case TEXT:
                 // with text
-                matcher = getViewInteractions(withText(selector), findOne);
+                matcher = getViewInteractions(root, withText(selector), findOne);
                 break;
             case ACCESSIBILITY_ID:
                 // with content description
-                matcher = getViewInteractions(withContentDescription(selector), findOne);
+                matcher = getViewInteractions(root, withContentDescription(selector), findOne);
                 break;
             case XPATH:
                 // If we're only looking for one item that matches xpath, pass it index 0 or else
                 // Espresso throws an AmbiguousMatcherException
                 if (findOne) {
-                    matcher = getViewInteractions(withXPath(selector, 0), true);
+                    matcher = getViewInteractions(root, withXPath(selector, 0), true);
                 } else {
-                    matcher = getViewInteractions(withXPath(selector), false);
+                    matcher = getViewInteractions(root, withXPath(selector), false);
                 }
                 break;
             default:
@@ -122,11 +147,17 @@ public class ViewFinder {
         return matcher;
     }
 
-    private static List<ViewInteraction> getViewInteractions(Matcher<View> matcher, boolean findOne) {
+    private static List<ViewInteraction> getViewInteractions(
+            @Nullable View root, Matcher<View> matcher, boolean findOne) {
         // If it's just one view we want, return a singleton list
         if (findOne) {
             try {
-                return Collections.singletonList(onView(withIndex(matcher, 0)));
+                if (root == null) {
+                    return Collections.singletonList(onView(withIndex(matcher, 0)));
+                }
+                return Collections.singletonList(
+                        onView(withIndex(matcher, 0)).inRoot(withDecorView(is(root)))
+                );
             } catch (NoMatchingViewException e) {
                 return Collections.emptyList();
             }
@@ -139,7 +170,13 @@ public class ViewFinder {
         int i = 0;
         do {
             try {
-                ViewInteraction viewInteraction = onView(withIndex(matcher, i++));
+                ViewInteraction viewInteraction;
+                if (root == null) {
+                    viewInteraction = onView(withIndex(matcher, i++));
+                } else {
+                    viewInteraction = onView(withIndex(matcher, i++))
+                            .inRoot(withDecorView(is(root)));
+                }
                 viewInteractions.add(viewInteraction);
             } catch (NoMatchingViewException e) {
                 return viewInteractions;
