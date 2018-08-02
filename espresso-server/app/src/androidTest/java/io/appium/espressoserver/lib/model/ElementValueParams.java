0f9180ef4dafcd9e0a4c7f68dfdc83c543a1cfe3
Dan Graham <dan@saucelabs.com>
2018-08-02 13:33:44 -0700

    Add appium element extensions /value and /replace_value (#194)

---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/model/ElementValueParams.java
@@ -0,0 +1,13 @@
+package io.appium.espressoserver.lib.model;
+
+public class ElementValueParams extends AppiumParams {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
