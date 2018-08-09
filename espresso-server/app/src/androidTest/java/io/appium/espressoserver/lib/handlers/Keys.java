884e7c235b7ff84ea0c1663db294449ae0513cd7
dan@saucelabs.com <dan@saucelabs.com>
2018-08-09 10:21:10 -0700

    Codacy fix

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/Keys.java
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/Keys.java
@@ -24,7 +24,6 @@ import java.util.List;
 
 import io.appium.espressoserver.lib.handlers.exceptions.AppiumException;
 import io.appium.espressoserver.lib.helpers.w3c.adapter.espresso.EspressoW3CActionAdapter;
-import io.appium.espressoserver.lib.helpers.w3c.models.Actions;
 import io.appium.espressoserver.lib.helpers.w3c.models.Actions.ActionsBuilder;
 import io.appium.espressoserver.lib.helpers.w3c.models.InputSource;
 import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.Action;
