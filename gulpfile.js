b83990427cacc6404a8c888e9c1c1c4a2c842b83
Isaac A. Murchie <isaac.appium@gmail.com>
2018-10-02 09:27:46 -0400

    Update appium-gulp-plugins (#235)
    * Update appium-gulp-plugins
    
    * Fix travis
    
    * Fix linting after rebase

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 /* eslint-disable */
 "use strict";
 
-var gulp = require('gulp'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+const gulp = require('gulp');
+const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-espresso-driver',
-  jscs: false,
   testTimeout: 120000,
   e2eTest: { android: true },
   eslint: true,
