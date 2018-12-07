7f2c154d9782ba59f2211aa532e63c85683d7e86
Rajdeep Varma <mail.rajvarma@gmail.com>
2018-12-07 11:06:00 +0000

    change Integer to int

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/MobileViewFlash.java
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/MobileViewFlash.java
@@ -11,14 +11,14 @@ import io.appium.espressoserver.lib.model.ViewFlashParams;
 
 public class MobileViewFlash implements RequestHandler<ViewFlashParams, Void> {
 
-    private static final Integer DURATION_MILLIS = 30;
-    private static final Integer REPEAT_COUNT = 15;
+    private static final int DURATION_MILLIS = 30;
+    private static final int REPEAT_COUNT = 15;
 
     @Override
     public Void handle(final ViewFlashParams params) throws AppiumException {
 
-        final Integer duration = params.getDurationMillis() == null ? DURATION_MILLIS : params.getDurationMillis();
-        final Integer repeatCount = params.getRepeatCount() == null ? REPEAT_COUNT : params.getRepeatCount();
+        final int duration = params.getDurationMillis() == null ? DURATION_MILLIS : params.getDurationMillis();
+        final int repeatCount = params.getRepeatCount() == null ? REPEAT_COUNT : params.getRepeatCount();
 
         final View view = Element.getViewById(params.getElementId());
         InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
