d5d279cdef946d359233d39b3ed9e941f73a5b31
dpgraham <dpgraham.85@gmail.com>
2019-01-15 12:00:20 -0800

    Rename symbol

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/GsonParserHelpers.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/GsonParserHelpers.kt
@@ -2,13 +2,12 @@ package io.appium.espressoserver.lib.helpers
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParseException
-import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
 
 class GsonParserHelpers {
 
     inline fun <reified T : Enum<T>> parseEnum(jsonObj: JsonObject, propName: String, helperMessage: String = ""): T? {
-        val direction = jsonObj.get(propName)
-        if (direction != null) {
+        val property = jsonObj.get(propName)
+        if (property != null) {
             val propValueAsString = jsonObj.get(propName).asString.toUpperCase()
             try {
                 return enumValueOf<T>(propValueAsString)
