222ba544794da8875fe2f44f135bd52c7aa713a2
dpgraham <dpgraham.85@gmail.com>
2019-01-17 17:24:45 -0800

    Wire up WebAtoms to use Reflection

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/WebAtoms.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/WebAtoms.kt
@@ -16,21 +16,17 @@
 
 package io.appium.espressoserver.lib.handlers
 
-import androidx.test.espresso.web.model.Atom
 import androidx.test.espresso.web.sugar.Web.WebInteraction
 import io.appium.espressoserver.lib.handlers.exceptions.AppiumException
 import io.appium.espressoserver.lib.model.web.WebAtomsParams
 import androidx.test.espresso.web.sugar.Web.onWebView
 import androidx.test.espresso.web.webdriver.DriverAtoms
-import androidx.test.espresso.web.webdriver.DriverAtoms.*
-import androidx.test.espresso.web.webdriver.Locator
 import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
 import io.appium.espressoserver.lib.helpers.AndroidLogger.logger
+import io.appium.espressoserver.lib.helpers.KReflectionUtils.invokeInstanceMethod
+import io.appium.espressoserver.lib.helpers.KReflectionUtils.invokeMethod
 import io.appium.espressoserver.lib.model.Element
 import io.appium.espressoserver.lib.viewmatcher.WithView.withView
-import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
-import kotlin.reflect.full.*
 
 class WebAtoms : RequestHandler<WebAtomsParams, Void> {
 
@@ -42,7 +38,7 @@ class WebAtoms : RequestHandler<WebAtomsParams, Void> {
 
         // Initialize onWebView with web view matcher (if webviewEl provided)
         if (webAtomsParams.webviewElement != null) {
-            logger.info("Initializing webView with webview el ${webAtomsParams.webviewElement}")
+            logger.info("Initializing webView interaction on webview with el: '${webAtomsParams.webviewElement}")
             val matcher = withView(Element.getViewById(webAtomsParams.webviewElement))
             webViewInteraction = onWebView(matcher)
         } else {
@@ -57,53 +53,23 @@ class WebAtoms : RequestHandler<WebAtomsParams, Void> {
 
         // Iterate through methodsChain and call the atoms
         for (method in webAtomsParams.methodChain) {
-            logger.info("Parsing web atom '${method.name}' with atom '${method.atom}'");
-            for (interactionFunction:KFunction<*> in webViewInteraction.javaClass.kotlin.memberFunctions) {
-                if (interactionFunction.name == method.name) {
-                    val paramClassifier = interactionFunction.parameters.get(1).type.classifier;
+            logger.info("Calling method '${method.name}' with atom '${method.atom}'");
 
-                    // TODO: Make a parameter matcher
-                    if (paramClassifier is KClass<*>) {
-                        if (!(paramClassifier.simpleName == "Atom")) {
-                            continue;
-                        }
-                    }
-                    val atomClass = DriverAtoms::class
-                    for (atomFunc in atomClass.functions) {
-                        if (atomFunc.name == method.atom.name) {
-                            val arr:Array<Any> = method.atom.args.toTypedArray()
+            logger.info("Calling atom '${method.atom.name!!}' with args '${method.atom.args.joinToString(", ")}'")
+            val atom = invokeMethod(DriverAtoms::class, method.atom.name!!, *method.atom.args.toTypedArray());
 
-                            // TODO: Make this more versatile. Should check expected parameters and provided parameters
-                            // and if expected is 'enum' use enumValueOf
-                            if (atomFunc.name == "findElement") {
-                                val firstArr = arr[0]
-                                if (firstArr is String) {
-                                    arr[0] = Locator.valueOf(firstArr.toUpperCase())
-                                    logger.info("Locator type is ${arr[0]}");
-                                }
-                                // TODO: Throw exception for bad Enum type
-                            }
-                            val atom = atomFunc.call(*arr)
+            logger.info("Calling interaction '${method.name}' with the atom")
+            var res:Any?
+            if (atom != null) {
+                res = invokeInstanceMethod(webViewInteraction, method.name, atom);
+            } else {
+                res = invokeInstanceMethod(webViewInteraction, method.name);
+            }
 
-                            try {
-                                logger.info("Calling '${interactionFunction.name}(${atomFunc.name}(${arr.joinToString(", ")}))")
-                                val res = interactionFunction.call(webViewInteraction, atom)
-
-                                if (res is WebInteraction<*>) {
-                                    webViewInteraction = res;
-                                } else {
-                                    // TODO: Throw an exception because interaction function returned wrong result
-                                    // This block will probably never be reached
-                                }
-                            } catch (rte:ReflectiveOperationException) {
-                                throw InvalidArgumentException("Could not execute web atom: '${method.atom}'. Reason: ${rte.cause}");
-                            }
-
-                        }
-                    }
-                    // TODO: Throw exception if atom function not found
-                }
-                // TODO: Throw exception if interaction function not found
+            if (res is WebInteraction<*>) {
+                webViewInteraction = res;
+            } else {
+                throw InvalidArgumentException("'${method.name}' does not return a 'WebViewInteraction' object");
             }
         }
 
