c057a08891a6230167cdb9203f8a9794f93fd4ac
dpgraham <dpgraham.85@gmail.com>
2019-02-25 17:54:05 -0800

    Try Azure now 554

--- azure-pipelines/start-emulator.sh
+++ azure-pipelines/start-emulator.sh
@@ -2,7 +2,7 @@
 
 # Install AVD files
 # TODO: Do not hardcode Android SDK versions
-# echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86'
+echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86'
 
 # Create emulator
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis;x86' --force
