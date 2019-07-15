f1c25c1caaa9e7c2772a11bf00fae108a4e7a792
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-07-15 10:29:47 +0900

    set do not suppress accessibility services for uiautomation

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/InteractionHelper.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/InteractionHelper.kt
@@ -1,6 +1,9 @@
 package io.appium.espressoserver.lib.helpers
 
+import android.app.UiAutomation
+import android.os.Build
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.Configurator
 import androidx.test.uiautomator.UiDevice
 
 object InteractionHelper {
@@ -9,6 +12,12 @@ object InteractionHelper {
     @Synchronized
     fun getUiDevice(): UiDevice {
         if (uiDevice == null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                // The flag is necessary not to stop running accessibility service
+                // https://github.com/appium/appium/issues/4910
+                // https://developer.android.com/reference/android/app/UiAutomation.html#FLAG_DONT_SUPPRESS_ACCESSIBILITY_SERVICES
+                Configurator.getInstance().uiAutomationFlags = UiAutomation.FLAG_DONT_SUPPRESS_ACCESSIBILITY_SERVICES
+            }
             uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         }
         return uiDevice!!
