7bdd643d1a81d4328a2c73125cbf4de0c31ecdb2
Daniel-Graham-SL1298 <Daniel-Graham-SL1298@MacBook-Pro.local>
2019-07-22 15:45:06 -0700

    Kotlin translations

---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/w3c/processor/ActionsProcessor.kt
@@ -0,0 +1,96 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.appium.espressoserver.lib.helpers.w3c.processor
+
+import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
+import io.appium.espressoserver.lib.handlers.exceptions.NotYetImplementedException
+import io.appium.espressoserver.lib.helpers.w3c.models.ActionObject
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.InputSourceType
+import io.appium.espressoserver.lib.helpers.w3c.state.ActiveInputSources
+import io.appium.espressoserver.lib.helpers.w3c.state.InputStateTable
+
+import io.appium.espressoserver.lib.helpers.w3c.processor.KeyProcessor.processKeyAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PauseProcessor.processNullAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PointerProcessor.processPointerAction
+
+object ActionsProcessor {
+
+    /**
+     * Follows algorithm "for process an input source action sequence" in section 17.3
+     */
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun processSourceActionSequence(inputSource: InputSource, activeInputSources: ActiveInputSources, inputStateTable: InputStateTable): List<ActionObject> {
+        // 1: Get the type
+        // 2: If type is not "key", "pointer", or "none", return an error
+        val type = inputSource.type
+                ?: throw InvalidArgumentException("'type' is required in input source and must be one of: pointer, key, none")
+
+
+        // 3: Get the ID from the input source
+        // 4: If id is undefined or is not a String, return error
+        val id = inputSource.id
+                ?: throw InvalidArgumentException("'id' in action cannot be null")
+
+
+        // 6: Let source be the input source in the list of active input sources where that input source’s input id matches id,
+        val activeSource = activeInputSources.getInputSource(inputSource)
+
+        // 7:  source is undefined:
+        if (activeSource == null) {
+            activeInputSources.addInputSource(inputSource)
+            inputStateTable.addInputState(inputSource.id, inputSource.defaultState)
+        } else {
+
+            // 8: If source's type does not match type return an error
+            if (activeSource.type != inputSource.type) {
+                throw InvalidArgumentException("Input type ${inputSource.type} does not match pre-existing input type '${activeSource.type}' in actions input source with id '${id}'")
+            }
+
+            // 9: If it's a pointer type, check that they match parameter types
+            if (activeSource.type == InputSourceType.POINTER && activeSource.pointerType != inputSource.pointerType) {
+                throw InvalidArgumentException("Pointer type ${inputSource.pointerType} does not match pre-existing pointer type '${activeSource.pointerType}' in actions input " +
+                        "source with id '${id}'");
+            }
+        }
+
+        // 10: Let action items be the result of getting a property named actions from action sequence
+        // 11: If action items is not an Array, return error
+        val actionItems = inputSource.actions
+                ?: throw InvalidArgumentException("'actions' array not provided in actions input source with id '${id}'")
+
+        // 12: Let actions be a new list
+        val actionObjects = arrayListOf<ActionObject>()
+
+        // 13: For each item in action items
+        var index = 0
+        for (action in actionItems) {
+            if (action == null) {
+                throw InvalidArgumentException("'actions[${index}]' did not provide a valid JSON object for actions input source with id '${id}'")
+            }
+            index++
+            when (inputSource.type) {
+                InputSourceType.NONE -> actionObjects.add(processNullAction(action, inputSource.type, id, index))
+                InputSourceType.POINTER -> actionObjects.add(processPointerAction(action, inputSource, id, index))
+                InputSourceType.KEY -> actionObjects.add(processKeyAction(action, inputSource.type, id, index))
+                else -> throw InvalidArgumentException("'actions[${index}]' of input source with id '${id}' did not provide a valid type")
+            }
+        }
+
+        return actionObjects
+    }
+}
---
+++ espresso-server/app/src/test/java/io/appium/espressoserver/test/helpers/w3c/ActionSequenceTest.kt
@@ -0,0 +1,227 @@
+package io.appium.espressoserver.test.helpers.w3c
+
+import com.google.gson.Gson
+
+import org.junit.Test
+
+import java.io.IOException
+import java.util.concurrent.ExecutionException
+
+import io.appium.espressoserver.lib.handlers.exceptions.AppiumException
+import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
+import io.appium.espressoserver.lib.handlers.exceptions.NotYetImplementedException
+import io.appium.espressoserver.lib.helpers.w3c.adapter.DummyW3CActionAdapter
+import io.appium.espressoserver.lib.helpers.w3c.models.ActionObject
+import io.appium.espressoserver.lib.helpers.w3c.models.ActionSequence
+import io.appium.espressoserver.lib.helpers.w3c.models.Actions
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.PointerType
+import io.appium.espressoserver.lib.helpers.w3c.models.Tick
+import io.appium.espressoserver.lib.helpers.w3c.state.ActiveInputSources
+import io.appium.espressoserver.lib.helpers.w3c.state.InputStateTable
+import io.appium.espressoserver.lib.helpers.w3c.state.PointerInputState
+import io.appium.espressoserver.test.assets.Helpers
+
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType.KEY_DOWN
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType.KEY_UP
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType.PAUSE
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType.POINTER_DOWN
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType.POINTER_MOVE
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.InputSourceType.KEY
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.InputSourceType.POINTER
+import io.appium.espressoserver.test.helpers.w3c.Helpers.assertFloatEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+
+class ActionSequenceTest {
+
+    @Test
+    @Throws(IOException::class, InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldPullOutPointerActionsInW3CActions() {
+        val multiTouchJson = Helpers.readAssetFile("multi-touch-actions.json")
+        val actions = Actions::class.java.cast(Gson().fromJson(multiTouchJson, Actions::class.java))
+        val actionSequence = ActionSequence(actions!!, ActiveInputSources(), InputStateTable())
+
+        // Tick #1 of 6
+        var tick = actionSequence.next()
+        var action = tick.next()
+        assertEquals(action.id, "finger1")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, POINTER_MOVE)
+        assertEquals(action.duration, 0f)
+        assertFloatEquals(action.x, 100f)
+        assertFloatEquals(action.y, 100f)
+
+        action = tick.next()
+        assertEquals(action.id, "finger2")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, POINTER_MOVE)
+        assertEquals(action.duration, 10f)
+        assertFloatEquals(action.x, 200f)
+        assertFloatEquals(action.y, 400f)
+
+        assertFalse(tick.hasNext())
+
+        // Tick #2 of 6
+        tick = actionSequence.next()
+        action = tick.next()
+        assertEquals(action.id, "finger1")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, POINTER_DOWN)
+        assertEquals(action.button.toLong(), 0)
+
+        action = tick.next()
+        assertEquals(action.id, "finger2")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, POINTER_DOWN)
+        assertEquals(action.button.toLong(), 0)
+
+        // Tick #3 of 6
+        tick = actionSequence.next()
+        action = tick.next()
+        assertEquals(action.id, "finger1")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, PAUSE)
+        assertEquals(action.button.toLong(), 0)
+
+        action = tick.next()
+        assertEquals(action.id, "finger2")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, PAUSE)
+        assertEquals(action.button.toLong(), 0)
+
+        assertFalse(tick.hasNext())
+
+        // Tick #4 of 6
+        tick = actionSequence.next()
+        action = tick.next()
+        assertEquals(action.id, "finger1")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, POINTER_MOVE)
+        assertEquals(action.duration, 1000f)
+        assertTrue(action.pointer == PointerType.TOUCH)
+        assertFloatEquals(action.x, 20f)
+        assertFloatEquals(action.y, 0f)
+
+        action = tick.next()
+        assertEquals(action.id, "finger2")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, POINTER_MOVE)
+        assertEquals(action.duration, 1000f)
+        assertEquals(action.origin!!.type, InputSource.POINTER)
+        assertFloatEquals(action.x, 50f)
+        assertFloatEquals(action.y, 0f)
+
+        assertFalse(tick.hasNext())
+
+        // Tick #5 of 6
+        tick = actionSequence.next()
+        action = tick.next()
+        assertEquals(action.id, "finger1")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, ActionType.POINTER_UP)
+        assertEquals(action.button.toLong(), 0)
+
+        action = tick.next()
+        assertEquals(action.id, "finger2")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, ActionType.POINTER_UP)
+        assertEquals(action.button.toLong(), 0)
+
+        assertFalse(tick.hasNext())
+
+        // Tick #6 of 6
+        tick = actionSequence.next()
+        action = tick.next()
+        assertEquals(action.id, "finger2")
+        assertEquals(action.type, POINTER)
+        assertEquals(action.subType, PAUSE)
+        assertEquals(action.duration, 0f)
+
+        assertFalse(tick.hasNext())
+        assertFalse(actionSequence.hasNext())
+    }
+
+
+    @Test
+    @Throws(IOException::class, InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldPullOutKeyActionsInW3CActions() {
+        val keyJson = Helpers.readAssetFile("key-actions.json")
+        val actions = Actions::class.java.cast(Gson().fromJson(keyJson, Actions::class.java))
+        val actionSequence = ActionSequence(actions!!, ActiveInputSources(), InputStateTable())
+
+        var action: ActionObject
+        var tick: Tick
+
+        val unicodeChar = Character.toString('\uE009')
+
+        val expectedSubTypes = arrayOf(KEY_DOWN, PAUSE, KEY_DOWN, KEY_UP, KEY_UP)
+        val expectedValue = arrayOf(unicodeChar, null, "s", unicodeChar, "s")
+
+
+        for (i in expectedSubTypes.indices) {
+            tick = actionSequence.next()
+            action = tick.next()
+            assertEquals(action.type, KEY)
+            assertEquals(action.subType, expectedSubTypes[i])
+            assertEquals(action.value, expectedValue[i])
+            assertEquals(action.id, "keyboard")
+            assertFalse(tick.hasNext())
+        }
+
+        assertFalse(actionSequence.hasNext())
+    }
+
+    @Test
+    @Throws(IOException::class, AppiumException::class, InterruptedException::class, ExecutionException::class)
+    fun shouldDispatchW3CPointerActions() {
+        class AlteredDummyAdapter : DummyW3CActionAdapter() {
+            override// Bump up viewport width so we don't get out of bounds issues
+            val viewportWidth: Long
+                get() = 251
+
+            override val viewportHeight: Long
+                get() = 401
+        }
+
+        val inputStateTable = InputStateTable()
+        val multiTouchJson = Helpers.readAssetFile("multi-touch-actions.json")
+        val actions = Actions::class.java.cast(Gson().fromJson(multiTouchJson, Actions::class.java))
+        val actionSequence = ActionSequence(actions!!, ActiveInputSources(), InputStateTable())
+
+        val timeBefore = System.currentTimeMillis()
+        actionSequence.dispatch(AlteredDummyAdapter(), inputStateTable)
+        val elapsedTime = System.currentTimeMillis() - timeBefore
+
+        // Must be greater than 2s because that's the total duration
+        assertTrue(elapsedTime >= 2000)
+
+        // Check that it's under 2.5s though to verify that it's not TOO long
+        assertTrue(elapsedTime < 2500)
+
+        val finger1State = inputStateTable.getInputState("finger1") as PointerInputState
+        val finger2State = inputStateTable.getInputState("finger2") as PointerInputState
+
+        assertFloatEquals(finger1State.x, 120f)
+        assertFloatEquals(finger1State.y, 100f)
+        assertFloatEquals(finger2State.x, 250f)
+        assertFloatEquals(finger2State.y, 400f)
+    }
+
+    @Test
+    @Throws(IOException::class, AppiumException::class, InterruptedException::class, ExecutionException::class)
+    fun shouldDispatchW3CKeyActions() {
+        val inputStateTable = InputStateTable()
+        val multiTouchJson = Helpers.readAssetFile("key-actions.json")
+        val actions = Actions::class.java.cast(Gson().fromJson(multiTouchJson, Actions::class.java))
+        val actionSequence = ActionSequence(actions!!, ActiveInputSources(), InputStateTable())
+
+        val timeBefore = System.currentTimeMillis()
+        actionSequence.dispatch(DummyW3CActionAdapter(), inputStateTable)
+        val elapsedTime = System.currentTimeMillis() - timeBefore
+        assertTrue(elapsedTime >= 500)
+        assertTrue(elapsedTime <= 600)
+    }
+}
