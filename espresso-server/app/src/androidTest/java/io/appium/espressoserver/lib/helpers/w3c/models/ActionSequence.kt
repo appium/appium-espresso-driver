afc71e08dea936bbc72b88ffeaa8cf4128262912
Dan Graham <dpgraham.85@gmail.com>
2019-07-23 21:16:42 -0700

    Kotlin translations W3C actions #2 (#472)

---
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/w3c/models/ActionSequence.kt
@@ -0,0 +1,117 @@
+package io.appium.espressoserver.lib.helpers.w3c.models
+
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.ExecutorCompletionService
+import java.util.concurrent.Executors
+
+import io.appium.espressoserver.lib.handlers.exceptions.AppiumException
+import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
+import io.appium.espressoserver.lib.handlers.exceptions.NotYetImplementedException
+import io.appium.espressoserver.lib.helpers.w3c.adapter.W3CActionAdapter
+import io.appium.espressoserver.lib.helpers.w3c.dispatcher.BaseDispatchResult
+import io.appium.espressoserver.lib.helpers.w3c.state.ActiveInputSources
+import io.appium.espressoserver.lib.helpers.w3c.state.InputStateTable
+
+import io.appium.espressoserver.lib.helpers.w3c.processor.ActionsProcessor.processSourceActionSequence
+
+/**
+ * The algorithm for extracting an action sequence from a request takes the JSON Object representing
+ * an action sequence, validates the input, and returns a data structure that is the transpose of
+ * the input JSON, such that the actions to be performed in a single tick are grouped together
+ *
+ * (Defined in 17.3 of spec 'extract an action sequence')
+ */
+class ActionSequence @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+constructor(actions: Actions, activeInputSources: ActiveInputSources,
+            inputStateTable: InputStateTable) : Iterator<Tick> {
+
+    private val ticks = arrayListOf<Tick>()
+    private var tickCounter = 0
+
+    init {
+        // Check if null to keep Codacy happy. It will never make it this far if it's null though.
+        val actionList = actions.actions
+        if (actionList != null) {
+            for (inputSource in actionList) {
+                var tickIndex = 0
+                val actionObjects = processSourceActionSequence(inputSource, activeInputSources, inputStateTable)
+                for (action in actionObjects) {
+                    if (ticks.size == tickIndex) {
+                        ticks.add(Tick())
+                    }
+                    val tick = ticks[tickIndex]
+                    tick.addAction(action)
+                    tickIndex++
+                }
+            }
+        }
+    }
+
+    override fun hasNext(): Boolean {
+        return tickCounter < ticks.size
+    }
+
+    override fun next(): Tick {
+        return ticks[tickCounter++]
+    }
+
+    /**
+     * Call the dispatchAll algorithm defined in 17.4
+     * @param adapter W3C ActionsPerformer adapter
+     * @param inputStateTable Input states for this session
+     * @throws AppiumException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Throws(AppiumException::class, InterruptedException::class, ExecutionException::class)
+    fun dispatch(adapter: W3CActionAdapter, inputStateTable: InputStateTable) {
+        var tickIndex = 0
+        for (tick in ticks) {
+            val timeAtBeginningOfTick = System.currentTimeMillis()
+            val tickDuration = tick.calculateTickDuration()
+
+            // 1. Dispatch all of the events
+            adapter.logger.info("Dispatching tick #${tickIndex + 1} of ${ticks.size}");
+            val callables = tick.dispatchAll(adapter, inputStateTable, tickDuration)
+            var callableCount = callables.size
+
+            adapter.sychronousTickActionsComplete()
+
+            // 2. Wait until the following conditions are all met:
+
+            //  2.1 Wait for any pending async operations
+            if (!callables.isEmpty()) {
+                val executor = Executors.newCachedThreadPool()
+                val completionService = ExecutorCompletionService<BaseDispatchResult>(executor)
+                for (callable in callables) {
+                    completionService.submit(callable)
+                }
+
+                var received = 0
+                while (received < callableCount) {
+                    val resultFuture = completionService.take() //blocks if none available
+                    val dispatchResult = resultFuture.get()
+                    dispatchResult.perform()
+                    if (dispatchResult.hasNext()) {
+                        callableCount++
+                        completionService.submit(dispatchResult.next)
+                    }
+                    received++
+                }
+            }
+
+            //  2.2 At least tick duration milliseconds have passed
+            val timeSinceBeginningOfTick = (System.currentTimeMillis() - timeAtBeginningOfTick).toFloat()
+            if (timeSinceBeginningOfTick < tickDuration) {
+                val timeToSleep = tickDuration - timeSinceBeginningOfTick
+                adapter.logger.info("Wait for tick to finish for ${timeToSleep} ms")
+                adapter.sleep(timeToSleep)
+            }
+
+            // 2.3 The UI thread is complete
+            adapter.waitForUiThread()
+
+            tickIndex++
+        }
+    }
+}
---
+++ espresso-server/app/src/test/java/io/appium/espressoserver/test/helpers/w3c/ProcessorTest.kt
@@ -0,0 +1,326 @@
+package io.appium.espressoserver.test.helpers.w3c
+
+
+import org.junit.Before
+import org.junit.Test
+
+import java.util.ArrayList
+
+import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
+import io.appium.espressoserver.lib.handlers.exceptions.NotYetImplementedException
+import io.appium.espressoserver.lib.helpers.w3c.models.ActionObject
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.Action
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.ActionType
+import io.appium.espressoserver.lib.helpers.w3c.models.InputSource.InputSourceType
+import io.appium.espressoserver.lib.helpers.w3c.state.ActiveInputSources
+import io.appium.espressoserver.lib.helpers.w3c.state.InputStateTable
+import io.appium.espressoserver.lib.helpers.w3c.state.KeyInputState
+
+import io.appium.espressoserver.lib.helpers.w3c.processor.ActionsProcessor.processSourceActionSequence
+import io.appium.espressoserver.lib.helpers.w3c.processor.KeyProcessor.processKeyAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PauseProcessor.processNullAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PauseProcessor.processPauseAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PointerProcessor.processPointerAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PointerProcessor.processPointerMoveAction
+import io.appium.espressoserver.lib.helpers.w3c.processor.PointerProcessor.processPointerUpOrDownAction
+import io.appium.espressoserver.test.helpers.w3c.Helpers.assertFloatEquals
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertNull
+import junit.framework.Assert.fail
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+
+class ProcessorTest {
+
+    private var pointerInputSource: InputSource? = null
+
+    @Before
+    fun before() {
+        pointerInputSource = InputSource()
+        pointerInputSource!!.type = InputSourceType.POINTER
+    }
+
+
+    @Test
+    fun shouldRejectNullIfNotPause() {
+        val action = Action()
+        action.setType(ActionType.POINTER_DOWN)
+        try {
+            processNullAction(action, InputSourceType.NONE, "any1", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("must be type 'pause'"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldPassNullIfPause() {
+        val action = Action()
+        action.type = ActionType.PAUSE
+        val actionObject = processNullAction(action, InputSourceType.NONE, "any1", 0)
+        assertEquals(actionObject.type, InputSourceType.NONE)
+        assertEquals(actionObject.subType, ActionType.PAUSE)
+    }
+
+    @Test
+    fun shouldRejectPauseIfNegativeDuration() {
+        val action = Action()
+        action.setDuration(-1f)
+        try {
+            processPauseAction(action, InputSourceType.NONE, "any3", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("'duration' be greater than or equal to 0"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldPassPauseWithNullDuration() {
+        val action = Action()
+        assertNull(action.duration)
+        val actionObject = processPauseAction(action, InputSourceType.NONE, "any", 0)
+        assertNull(actionObject.duration)
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldPassPauseWithDurationSet() {
+        val action = Action()
+        action.setDuration(10f)
+        val actionObject = processPauseAction(action, InputSourceType.NONE, "any", 0)
+        assertEquals(actionObject.duration, 10f)
+    }
+
+    @Test
+    fun shouldRejectNullIfTypeNotPause() {
+        val action = Action()
+        try {
+            processPauseAction(action, InputSourceType.NONE, "any", 0)
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("type 'pause'"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldProcessNullAsPauseAction() {
+        val action = Action()
+        action.setDuration(100f)
+        action.type = ActionType.PAUSE
+        processPauseAction(action, InputSourceType.NONE, "any", 0)
+        assertEquals(action.duration, 100f)
+    }
+
+    @Test
+    fun shouldRejectPointerMoveIfNegativeDuration() {
+        val action = Action()
+        action.setDuration(-1f)
+        try {
+            processPointerMoveAction(action, InputSourceType.POINTER, "any", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("'duration' be greater than or equal to 0"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldPassValidPointerMove() {
+        val action = Action()
+        action.x = 100f
+        action.y = 200f
+        action.setDuration(300f)
+        val actionObject = processPointerMoveAction(action, InputSourceType.POINTER, "any", 0)
+        assertFloatEquals(actionObject.x, 100f)
+        assertFloatEquals(actionObject.y, 200f)
+        assertEquals(actionObject.duration, 300f)
+    }
+
+    @Test
+    fun shouldRejectPointerUpOrDownIfButtonNegative() {
+        val action = Action()
+        action.button = -100
+        try {
+            processPointerUpOrDownAction(action, InputSourceType.POINTER, "any", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("property 'button' must be greater than or equal to 0"))
+        }
+
+    }
+
+    @Test
+    fun shouldRejectKeyIfNotValidType() {
+        val action = Action()
+        action.setType(ActionType.POINTER_DOWN)
+        try {
+            processKeyAction(action, InputSourceType.KEY, "any", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("has an invalid type"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldProcessKeyAsPauseIfPause() {
+        val action = Action()
+        action.type = ActionType.PAUSE
+        action.setDuration(300f)
+        val actionObject = processKeyAction(action, InputSourceType.KEY, "any", 0)
+        assertEquals(actionObject.duration, 300f)
+        assertEquals(actionObject.subType, ActionType.PAUSE)
+        assertEquals(actionObject.type, InputSourceType.KEY)
+    }
+
+    @Test
+    fun shouldRejectKeyIfNotUnicode() {
+        val action = Action()
+        action.setType(ActionType.KEY_DOWN)
+        action.setValue("asdfafsd")
+        try {
+            processKeyAction(action, InputSourceType.KEY, "any", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("Must be a unicode point"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class)
+    fun shouldPassKeyIfUnicode() {
+        val action = Action()
+        action.type = ActionType.KEY_DOWN
+        val value = Character.toString('\uE9F0')
+        action.value = value
+        val actionObject = processKeyAction(action, InputSourceType.POINTER, "any", 0)
+        assertEquals(actionObject.value, value)
+        assertEquals(actionObject.subType, ActionType.KEY_DOWN)
+    }
+
+    @Test
+    @Throws(NotYetImplementedException::class)
+    fun shouldRejectInvalidPointerType() {
+        val action = Action()
+        action.setType(ActionType.KEY_DOWN)
+        try {
+            processPointerAction(action, pointerInputSource, "any", 0)
+            fail("expected exception was not occured.")
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("has an invalid type"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldProcessPointerAsPauseIfPause() {
+        val action = Action()
+        action.type = ActionType.PAUSE
+        action.setDuration(300f)
+        val actionObject = processPointerAction(action, pointerInputSource, "any", 0)
+        assertEquals(actionObject.duration, 300f)
+        assertEquals(actionObject.subType, ActionType.PAUSE)
+        assertEquals(actionObject.type, InputSourceType.POINTER)
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldProcessPointerAsPointerMove() {
+        val action = Action()
+        action.type = ActionType.POINTER_MOVE
+        val actionObject = processPointerAction(action, pointerInputSource, "any", 0)
+        assertEquals(actionObject.subType, ActionType.POINTER_MOVE)
+        assertEquals(actionObject.type, InputSourceType.POINTER)
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldProcessPointerAsPointerUp() {
+        val action = Action()
+        action.type = ActionType.POINTER_UP
+        val actionObject = processPointerAction(action, pointerInputSource, "any", 0)
+        assertEquals(actionObject.subType, ActionType.POINTER_UP)
+        assertEquals(actionObject.type, InputSourceType.POINTER)
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldProcessPointerAsPointerDown() {
+        val action = Action()
+        action.type = ActionType.POINTER_DOWN
+        val actionObject = processPointerAction(action, pointerInputSource, "any", 0)
+        assertEquals(actionObject.subType, ActionType.POINTER_DOWN)
+        assertEquals(actionObject.type, InputSourceType.POINTER)
+    }
+
+    @Test
+    @Throws(NotYetImplementedException::class)
+    fun shouldNotPassProcessorIfNoType() {
+        val inputSource = InputSource()
+        try {
+            processSourceActionSequence(inputSource, ActiveInputSources(), InputStateTable())
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("'type' is required in input source"))
+        }
+
+    }
+
+    @Test
+    @Throws(NotYetImplementedException::class)
+    fun shouldNotPassProcessorIfNoId() {
+        val inputSource = InputSource()
+        inputSource.setType(InputSourceType.KEY)
+        try {
+            processSourceActionSequence(inputSource, ActiveInputSources(), InputStateTable())
+        } catch (ie: InvalidArgumentException) {
+            assertTrue(ie.message!!.contains("'id' in action cannot be null"))
+        }
+
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldCreateNewEntryInActiveStateTables() {
+        val activeInputSources = ActiveInputSources()
+        val inputStateTable = InputStateTable()
+        val inputSource = InputSource()
+        inputSource.actions = ArrayList()
+        inputSource.type = InputSourceType.KEY
+        inputSource.id = "anything"
+        assertFalse(activeInputSources.hasInputSource("anything"))
+        processSourceActionSequence(inputSource, activeInputSources, inputStateTable)
+        assertEquals(activeInputSources.getInputSource("anything"), inputSource)
+        assertTrue(inputStateTable.getInputState("anything") is KeyInputState)
+    }
+
+    @Test
+    @Throws(InvalidArgumentException::class, NotYetImplementedException::class)
+    fun shouldReturnActionObjectsFromProcessor() {
+        val activeInputSources = ActiveInputSources()
+        val inputStateTable = InputStateTable()
+        val inputSource = InputSource()
+        val actions = ArrayList<Action>()
+        val action = Action()
+        action.type = ActionType.POINTER_DOWN
+        action.button = 0
+        actions.add(action)
+        inputSource.actions = actions
+        inputSource.type = InputSourceType.POINTER
+        inputSource.id = "anything"
+        val actionObjects = processSourceActionSequence(inputSource, activeInputSources, inputStateTable)
+        assertEquals(actionObjects.size.toLong(), 1)
+        val actionObject = actionObjects[0]
+        assertEquals(actionObject.subType, ActionType.POINTER_DOWN)
+        assertEquals(actionObject.type, InputSourceType.POINTER)
+    }
+}
