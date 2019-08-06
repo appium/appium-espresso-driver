57b79eff77fad5877eefc8b7d024ac69d1ec714d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-06 17:39:43 +0900

    fix send keys for multi byte string

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/SendKeys.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/SendKeys.kt
@@ -58,8 +58,9 @@ class SendKeys : RequestHandler<TextParams, Void?> {
         } catch (e: PerformException) {
             throw InvalidElementStateException("sendKeys", params.elementId, e)
         } catch (e: RuntimeException) {
+
             e.message?.let {
-                if (it.contains("IME does not understand how to translate")) throw e
+                if (!it.contains("IME does not understand how to translate")) throw e
             }
             params.text?.let {
                 value = it
