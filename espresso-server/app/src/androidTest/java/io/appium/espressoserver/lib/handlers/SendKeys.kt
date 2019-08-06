67acaa5c02bf7c90cdc4c2ac9228cae771f8ebd9
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-06 17:48:50 +0900

    add a test for multi byte string

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/SendKeys.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/SendKeys.kt
@@ -58,7 +58,6 @@ class SendKeys : RequestHandler<TextParams, Void?> {
         } catch (e: PerformException) {
             throw InvalidElementStateException("sendKeys", params.elementId, e)
         } catch (e: RuntimeException) {
-
             e.message?.let {
                 if (!it.contains("IME does not understand how to translate")) throw e
             }
