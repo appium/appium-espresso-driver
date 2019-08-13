d3fd61a378244879b764bb05d317dbd0a21b5c95
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-13 21:27:39 +0900

    turn companion object in clipboarddatatype

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/model/ClipboardDataType.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/model/ClipboardDataType.kt
@@ -6,11 +6,10 @@ enum class ClipboardDataType {
     PLAINTEXT;
 
     companion object {
-        private fun supportedDataTypes(): List<String> = values().map { it.toString().toLowerCase() }
-
         fun invalidClipboardDataType(contentType: String?) : InvalidArgumentException {
             return InvalidArgumentException(
-                "Only ${supportedDataTypes()} content types are supported. '$contentType' is given instead"
+                "Only ${values().map { it.toString().toLowerCase() }} content types are supported. " +
+                        "'$contentType' is given instead"
             )
         }
     }
