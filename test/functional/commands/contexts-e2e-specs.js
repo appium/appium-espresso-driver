55b21c8d1e7dd9c5ae74905d6aee9f9a18aee9f3
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-14 21:59:47 +0900

    skip 23 since it has 44 instead of 43 for now

--- test/functional/commands/contexts-e2e-specs.js
+++ test/functional/commands/contexts-e2e-specs.js
@@ -23,6 +23,11 @@ describe('context', function () {
   });
 
   it('should get contexts and set them without errors', async function () {
+    if (process.env.ANDROID_SDK_VERSION === '23') {
+      // Latest 23 emulator has chrome '44.0.2403' instead of '43.0.2357'
+      return;
+    }
+
     const viewContexts = await driver.contexts();
 
     await driver.currentContext().should.eventually.eql(viewContexts[0]);
