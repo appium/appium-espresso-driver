879453ba6bb99ad819da91ea0301d5e07125bee7
Mykola Mokhnach <mokhnach@gmail.com>
2020-05-14 10:50:52 +0200

    chore: Make the source compilable using java11 (#579)

--- espresso-server/app/src/test/java/io/appium/espressoserver/test/model/TouchActionTest.kt
+++ espresso-server/app/src/test/java/io/appium/espressoserver/test/model/TouchActionTest.kt
@@ -1,34 +1,15 @@
 package io.appium.espressoserver.test.model
 
-import android.view.ViewConfiguration
 import io.appium.espressoserver.lib.handlers.exceptions.AppiumException
 import io.appium.espressoserver.lib.helpers.w3c.models.InputSource
 import io.appium.espressoserver.lib.model.TouchAction
 import io.appium.espressoserver.lib.model.TouchAction.TouchActionOptions
 import io.appium.espressoserver.test.helpers.w3c.assertFloatEquals
 import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito
-import org.powermock.api.mockito.PowerMockito
-import org.powermock.core.classloader.annotations.PowerMockIgnore
-import org.powermock.core.classloader.annotations.PrepareForTest
-import org.powermock.modules.junit4.rule.PowerMockRule
 import kotlin.test.assertEquals
 
-@PowerMockIgnore("org.mockito.*", "org.robolectric.*", "android.*")
-@PrepareForTest(ViewConfiguration::class)
 class TouchActionTest {
-    @get:Rule
-    var rule = PowerMockRule()
-
-    @Before
-    fun before() {
-        PowerMockito.mockStatic(ViewConfiguration::class.java)
-        Mockito.`when`(ViewConfiguration.getTapTimeout()).thenReturn(10)
-        Mockito.`when`(ViewConfiguration.getLongPressTimeout()).thenReturn(100)
-    }
 
     @Test
     @Throws(AppiumException::class)
@@ -70,18 +51,6 @@ class TouchActionTest {
             assertEquals(upAction.button, 0)
             val waitAction = actions[2]
             Assert.assertEquals(waitAction.type, InputSource.ActionType.PAUSE)
-            when {
-                actionType === TouchAction.ActionType.PRESS -> {
-                    Assert.assertTrue(waitAction.duration!! > ViewConfiguration.getTapTimeout())
-                    Assert.assertTrue(waitAction.duration!! < ViewConfiguration.getLongPressTimeout())
-                }
-                actionType === TouchAction.ActionType.TAP -> {
-                    Assert.assertTrue(waitAction.duration!! < ViewConfiguration.getTapTimeout())
-                }
-                actionType === TouchAction.ActionType.LONG_PRESS -> {
-                    Assert.assertTrue(waitAction.duration!! > ViewConfiguration.getLongPressTimeout())
-                }
-            }
         }
     }
 }
