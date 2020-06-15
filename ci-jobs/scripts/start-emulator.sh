2d78ee6b58bf4cbd211872c33ba2e1df80b3a3bf
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-06-15 12:23:53 +0900

    remove _64 for api level 30

--- ci-jobs/scripts/start-emulator.sh
+++ ci-jobs/scripts/start-emulator.sh
@@ -4,7 +4,7 @@
 # with some changes
 
 # Install AVD files
-declare -r emulator="system-images;android-$ANDROID_SDK_VERSION;default;x86_64"
+declare -r emulator="system-images;android-$ANDROID_SDK_VERSION;default;x86"
 declare -r ANDROID_AVD=test
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install "$emulator"
 
