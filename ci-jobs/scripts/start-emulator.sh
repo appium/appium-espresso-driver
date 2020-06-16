6b27238a2fa4b7a6675371dc82f6b557bc61dd80
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-06-16 23:11:09 +0900

    print available images...

--- ci-jobs/scripts/start-emulator.sh
+++ ci-jobs/scripts/start-emulator.sh
@@ -8,6 +8,7 @@ declare -r emulator="system-images;android-$ANDROID_SDK_VERSION;google_apis;x86"
 declare -r ANDROID_AVD=test
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install "$emulator"
 
+$ANDROID_HOME/tools/bin/sdkmanager list
 $ANDROID_HOME/tools/bin/avdmanager list
 
 # Create emulator
