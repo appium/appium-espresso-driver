0f06c1db2c3277f7cdda4cffe268792248b471e6
Mykola Mokhnach <mokhnach@gmail.com>
2021-09-22 11:13:05 +0200

    Remove extra import

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/SizeHelpers.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/helpers/SizeHelpers.kt
@@ -20,7 +20,6 @@ import android.content.Context
 import android.graphics.Rect
 import android.view.WindowManager
 import androidx.test.core.app.ApplicationProvider
-import java.lang.IllegalStateException
 
 fun getCurrentWindowRect(): Rect {
     val context = ApplicationProvider.getApplicationContext<Context>()
