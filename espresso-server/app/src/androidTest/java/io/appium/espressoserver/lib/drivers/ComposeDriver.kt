92ed5da9de326c0fd57fb6878a85e7e0fe5f758c
Rajdeep Varma <mail.rajvarma@gmail.com>
2021-10-05 17:27:50 +0100

    Fix error message condition when text is not available

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/drivers/ComposeDriver.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/drivers/ComposeDriver.kt
@@ -69,7 +69,7 @@ class ComposeDriver : AppDriver {
     // https://developer.android.com/jetpack/compose/semantics#merged-vs-unmerged
     override fun getText(params: AppiumParams): String {
         val config = getSemanticsNode(params.elementId!!).config
-        if (!(config.contains(SemanticsProperties.Text) || config[SemanticsProperties.Text].isEmpty())) throw InvalidElementStateException()
+        if (!(config.contains(SemanticsProperties.Text)) || config[SemanticsProperties.Text].isEmpty()) throw InvalidElementStateException()
         return config[SemanticsProperties.Text][0].text
     }
 
