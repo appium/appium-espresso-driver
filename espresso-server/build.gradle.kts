9a779104939d9424d4619cae4a3318df008cda3a
Rajdeep <mail.rajvarma@gmail.com>
2022-03-14 16:49:21 +0000

    feat: make compose and kotlin dependencies version configurable (#760)

--- espresso-server/build.gradle.kts
+++ espresso-server/build.gradle.kts
@@ -1,20 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-
-    val appiumKotlin =
-        properties.getOrDefault("appiumKotlin", "1.5.10")
-
-    val appiumAndroidGradlePlugin =
-        properties.getOrDefault("appiumAndroidGradlePlugin", "7.0.3")
+    extra.apply {
+        set("appiumKotlin", properties.getOrDefault("appiumKotlin", "1.5.10"))
+        set(
+            "appiumAndroidGradlePlugin",
+            properties.getOrDefault("appiumAndroidGradlePlugin", "7.0.3")
+        )
+    }
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$appiumKotlin")
-        classpath("com.android.tools.build:gradle:$appiumAndroidGradlePlugin")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["appiumKotlin"]}")
+        classpath("com.android.tools.build:gradle:${rootProject.extra["appiumAndroidGradlePlugin"]}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
