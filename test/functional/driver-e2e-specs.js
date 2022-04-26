7f0fa8dbd1beb2aceb767d873449c2b257650166
Mykola Mokhnach <mokhnach@gmail.com>
2022-04-26 17:49:03 +0200

    refactor: Update some e2e tests for wdio compatibility

--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -1,22 +1,26 @@
 import path from 'path';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import remote from 'webdriverio';
-import axios from 'axios';
+import { remote } from 'webdriverio';
+// import axios from 'axios';
 import { HOST, PORT } from './helpers/session';
-import { APIDEMO_CAPS, GPS_CAPS } from './desired';
+import { APIDEMO_CAPS, amendCapabilities } from './desired';
 import { startServer } from '../..';
 
 
 chai.should();
 chai.use(chaiAsPromised);
 
+const COMMON_REMOTE_OPTIONS = {
+  hostname: HOST,
+  port: PORT,
+};
+
 describe('EspressoDriver', function () {
   let driver;
   let server;
   before(async function () {
     server = await startServer(PORT, HOST);
-    driver = await remote();
   });
   after(async function () {
     try {
@@ -27,141 +31,148 @@ describe('EspressoDriver', function () {
     describe('success', function () {
       afterEach(async function () {
         try {
-          await driver.quit();
+          await driver.deleteSession();
         } catch (ign) {}
+        driver = null;
       });
 
       it('should start android session focusing on default activity', async function () {
-        let status = await driver.init(APIDEMO_CAPS);
-
-        status[1].app.should.eql(APIDEMO_CAPS.app);
-
-        let activity = await driver.getCurrentDeviceActivity();
-        activity.should.equal('.ApiDemos');
+        driver = await remote({
+          ...COMMON_REMOTE_OPTIONS,
+          capabilities: APIDEMO_CAPS,
+        });
+        await driver.getCurrentActivity().should.eventually.equal('.ApiDemos');
       });
       it('should start android session focusing on specified activity', async function () {
         // for now the activity needs to be fully qualified
-        let status = await driver.init(Object.assign({
-          appActivity: 'io.appium.android.apis.accessibility.AccessibilityNodeProviderActivity'
-        }, APIDEMO_CAPS));
-
-        status[1].app.should.eql(APIDEMO_CAPS.app);
-
-        let activity = await driver.getCurrentDeviceActivity();
-        activity.should.equal('.accessibility.AccessibilityNodeProviderActivity');
-      });
-      // TODO: Replace GPS app with something that doesn't pop up a warning modal
-      it.skip('should start subsequent sessions with different apps', async function () {
-        let status = await driver.init(GPS_CAPS);
-
-        status[1].app.should.eql(GPS_CAPS.app);
-
-        let activity = await driver.getCurrentDeviceActivity();
-        activity.should.equal('.GPSTest');
-
-        await driver.quit();
-
-        status = await driver.init(APIDEMO_CAPS);
-
-        status[1].app.should.eql(APIDEMO_CAPS.app);
-
-        activity = await driver.getCurrentDeviceActivity();
-        activity.should.equal('.ApiDemos');
+        driver = await remote({
+          ...COMMON_REMOTE_OPTIONS,
+          capabilities: amendCapabilities(APIDEMO_CAPS, {
+            'appium:appActivity': 'io.appium.android.apis.accessibility.AccessibilityNodeProviderActivity'
+          }),
+        });
+        await driver.getCurrentActivity().should.eventually.equal('.accessibility.AccessibilityNodeProviderActivity');
       });
     });
     describe('failure', function () {
       it('should reject start session for non-existent activity', async function () {
         // for now the activity needs to be fully qualified
-        await driver.init(Object.assign({
-          appActivity: 'io.appium.android.apis.some.fake.Activity'
-        }, APIDEMO_CAPS)).should.eventually.be.rejected;
+        await remote({
+          ...COMMON_REMOTE_OPTIONS,
+          capabilities: amendCapabilities(APIDEMO_CAPS, {
+            'appium:appActivity': 'io.appium.android.apis.some.fake.Activity'
+          }),
+        }).should.eventually.be.rejected;
       });
       it('should reject opening of appPackage with incorrect signature', async function () {
-        await driver.init(Object.assign({
-          appPackage: 'com.android.settings',
-        }, APIDEMO_CAPS)).should.eventually.be.rejected;
+        await remote({
+          ...COMMON_REMOTE_OPTIONS,
+          capabilities: amendCapabilities(APIDEMO_CAPS, {
+            'appium:appActivity': 'com.android.settings'
+          }),
+        }).should.eventually.be.rejected;
       });
       it('should reject start session for internet permissions not set', async function () {
         // for now the activity needs to be fully qualified
-        await driver.init(Object.assign({}, APIDEMO_CAPS, {
-          app: path.resolve('test', 'assets', 'ContactManager.apk'),
-        })).should.eventually.be.rejectedWith(/INTERNET/);
+        await remote({
+          ...COMMON_REMOTE_OPTIONS,
+          capabilities: amendCapabilities(APIDEMO_CAPS, {
+            'appium:app': path.resolve('test', 'assets', 'ContactManager.apk')
+          }),
+        }).should.eventually.be.rejectedWith(/INTERNET/);
       });
     });
   });
   describe('.startActivity', function () {
     afterEach(async function () {
       try {
-        await driver.quit();
+        await driver.deleteSession();
       } catch (ign) {}
+      driver = null;
     });
     it('should start activity by name', async function () {
-      await driver.init(APIDEMO_CAPS);
-      await driver.startActivity({
-        appPackage: 'io.appium.android.apis',
-        appActivity: '.accessibility.AccessibilityNodeProviderActivity',
+      driver = await remote({
+        ...COMMON_REMOTE_OPTIONS,
+        capabilities: APIDEMO_CAPS,
       });
-      await driver.getCurrentDeviceActivity().should.eventually.eql('.accessibility.AccessibilityNodeProviderActivity');
+      await driver.startActivity(
+        'io.appium.android.apis',
+        '.accessibility.AccessibilityNodeProviderActivity',
+      );
+      await driver.getCurrentActivity().should.eventually.eql('.accessibility.AccessibilityNodeProviderActivity');
     });
     it('should start activity by fully-qualified name', async function () {
-      await driver.init(APIDEMO_CAPS);
-      await driver.startActivity({
-        appPackage: 'io.appium.android.apis',
-        appActivity: 'io.appium.android.apis.accessibility.AccessibilityNodeProviderActivity',
+      driver = await remote({
+        ...COMMON_REMOTE_OPTIONS,
+        capabilities: APIDEMO_CAPS,
       });
-      await driver.getCurrentDeviceActivity().should.eventually.eql('.accessibility.AccessibilityNodeProviderActivity');
+      await driver.startActivity(
+        'io.appium.android.apis',
+        'io.appium.android.apis.accessibility.AccessibilityNodeProviderActivity',
+      );
+      await driver.getCurrentActivity().should.eventually.eql('.accessibility.AccessibilityNodeProviderActivity');
     });
   });
 
   describe('.reset', function () {
     afterEach(async function () {
       try {
-        await driver.quit();
+        await driver.deleteSession();
       } catch (ign) {}
+      driver = null;
     });
     it('should raise an error for resetApp', async function () {
-      await driver.init(APIDEMO_CAPS);
-      await driver.resetApp(APIDEMO_CAPS).should.eventually.be.rejectedWith(/Please quit the session and create a new one/);
-    });
-  });
-
-  describe('keys', function () {
-    beforeEach(async function () {
-      await driver.init({
-        ...APIDEMO_CAPS,
-        appActivity: 'io.appium.android.apis.view.AutoComplete1',
-        autoGrantPermissions: true,
+      driver = await remote({
+        ...COMMON_REMOTE_OPTIONS,
+        capabilities: APIDEMO_CAPS,
       });
-    });
-    afterEach(async function () {
-      await driver.quit();
-    });
-    it('should send keys to focused-on element', async function () {
-      await driver.keys('Hello World!'.split(''));
-      const editEl = await driver.elementByXPath('//android.widget.AutoCompleteTextView');
-      await editEl.text().should.eventually.equal('Hello World!');
-      await editEl.clear();
-    });
-
-    it('should do long press keycode', async function () {
-      const KEYCODE_G = 35;
-      const META_SHIFT_MASK = 193;
-      let sessionId = await driver.getSessionId();
-
-      const endpoints = ['long_press_keycode', 'press_keycode'];
-      for (let endpoint of endpoints) {
-        await axios({
-          method: 'POST',
-          url: `http://${HOST}:${PORT}/wd/hub/session/${sessionId}/appium/device/${endpoint}`,
-          data: {
-            keycode: KEYCODE_G,
-            metastate: 0 | META_SHIFT_MASK,
-          },
-        });
-        const editEl = await driver.elementByXPath('//android.widget.AutoCompleteTextView');
-        await editEl.text().should.eventually.equal(endpoint === 'press_keycode' ? 'G' : 'GG');
-        await editEl.clear();
-      }
+      await driver.resetApp().should.eventually.be.rejectedWith(/Please quit the session and create a new one/);
     });
   });
+
+  // TODO: Update tests for wdio compatibility
+  // describe('keys', function () {
+  //   beforeEach(async function () {
+  //     driver = await remote({
+  //       ...COMMON_REMOTE_OPTIONS,
+  //       capabilities: amendCapabilities(APIDEMO_CAPS, {
+  //         'appium:appActivity': 'io.appium.android.apis.view.AutoComplete1',
+  //         'appium:autoGrantPermissions': true,
+  //       })
+  //     });
+  //   });
+  //   afterEach(async function () {
+  //     try {
+  //       await driver.deleteSession();
+  //     } catch (ign) {}
+  //     driver = null;
+  //   });
+  //   it('should send keys to focused-on element', async function () {
+  //     await driver.keys('Hello World!'.split(''));
+  //     const editEl = await driver.elementByXPath('//android.widget.AutoCompleteTextView');
+  //     await editEl.text().should.eventually.equal('Hello World!');
+  //     await editEl.clear();
+  //   });
+
+  //   it('should do long press keycode', async function () {
+  //     const KEYCODE_G = 35;
+  //     const META_SHIFT_MASK = 193;
+  //     let sessionId = await driver.getSessionId();
+
+  //     const endpoints = ['long_press_keycode', 'press_keycode'];
+  //     for (let endpoint of endpoints) {
+  //       await axios({
+  //         method: 'POST',
+  //         url: `http://${HOST}:${PORT}/wd/hub/session/${sessionId}/appium/device/${endpoint}`,
+  //         data: {
+  //           keycode: KEYCODE_G,
+  //           metastate: 0 | META_SHIFT_MASK,
+  //         },
+  //       });
+  //       const editEl = await driver.elementByXPath('//android.widget.AutoCompleteTextView');
+  //       await editEl.text().should.eventually.equal(endpoint === 'press_keycode' ? 'G' : 'GG');
+  //       await editEl.clear();
+  //     }
+  //   });
+  // });
 });
