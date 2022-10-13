31f0ed5667b1f0ca0447b278f3fd85ba99a70e8e
Mykola Mokhnach <mokhnach@gmail.com>
2022-10-13 19:49:41 +0200

    Fix invocation

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/MobileBackdoor.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/MobileBackdoor.kt
@@ -1,6 +1,5 @@
 package io.appium.espressoserver.lib.handlers
 
-import io.appium.espressoserver.lib.handlers.exceptions.AppiumException
 import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
 import io.appium.espressoserver.lib.helpers.ActivityHelpers
 import io.appium.espressoserver.lib.helpers.InvocationOperation
@@ -11,7 +10,6 @@ import io.appium.espressoserver.lib.model.MobileBackdoorParams
 
 class MobileBackdoor : RequestHandler<MobileBackdoorParams, Any?> {
 
-    @Throws(AppiumException::class)
     override fun handleInternal(params: MobileBackdoorParams): Any? {
         params.target?.let {target ->
             val activity = ActivityHelpers.currentActivity
@@ -21,7 +19,9 @@ class MobileBackdoor : RequestHandler<MobileBackdoorParams, Any?> {
             val result = when (target) {
                 InvocationTarget.ACTIVITY -> invokeBackdoorMethods(activity, ops)
                 InvocationTarget.APPLICATION -> invokeBackdoorMethods(activity.application, ops)
-                InvocationTarget.ELEMENT -> invokeBackdoorMethods(EspressoElement.getCachedViewStateById(params.targetElement), ops)
+                InvocationTarget.ELEMENT -> invokeBackdoorMethods(
+                    EspressoElement.getCachedViewStateById(params.targetElement).view, ops
+                )
                 else -> throw InvalidArgumentException("target cannot be '$target'")
             } ?: return null
 
@@ -38,12 +38,11 @@ class MobileBackdoor : RequestHandler<MobileBackdoorParams, Any?> {
         throw InvalidArgumentException("Target must not be empty and must be of type: 'activity', 'application'")
     }
 
-    @Throws(AppiumException::class)
+    @Suppress("RedundantNullableReturnType")
     private fun invokeBackdoorMethods(invokeOn: Any, ops: List<InvocationOperation>): Any? {
         return ops.fold(invokeOn) { invocationTarget, operation -> operation.apply(invocationTarget) }
     }
 
-    @Throws(InvalidArgumentException::class)
     private fun getBackdoorOperations(params: MobileBackdoorParams): List<InvocationOperation> {
         return params.methods.map {method ->
             InvocationOperation(method.name, method.arguments, method.argumentTypes)
