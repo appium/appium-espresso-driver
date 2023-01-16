97d20643e8513dc2d8a94b2edae6110f71bb79d0
Mykola Mokhnach <mokhnach@gmail.com>
2023-01-16 22:39:37 +0100

    feat: Add swipe support in Compose mode (#850)

--- espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/MobileSwipe.kt
+++ espresso-server/app/src/androidTest/java/io/appium/espressoserver/lib/handlers/MobileSwipe.kt
@@ -16,12 +16,17 @@
 
 package io.appium.espressoserver.lib.handlers
 
+import androidx.compose.ui.test.performGesture
+import androidx.compose.ui.test.swipeDown
+
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
+import androidx.compose.ui.test.swipeUp
 import androidx.test.espresso.UiController
 import androidx.test.espresso.action.GeneralSwipeAction
-import androidx.test.espresso.action.ViewActions.*
-import io.appium.espressoserver.lib.handlers.exceptions.AppiumException
 import io.appium.espressoserver.lib.handlers.exceptions.InvalidArgumentException
 import io.appium.espressoserver.lib.helpers.AndroidLogger
+import io.appium.espressoserver.lib.helpers.getNodeInteractionById
 import io.appium.espressoserver.lib.model.EspressoElement
 import io.appium.espressoserver.lib.model.MobileSwipeParams
 import io.appium.espressoserver.lib.model.MobileSwipeParams.Direction.*
@@ -31,35 +36,38 @@ import io.appium.espressoserver.lib.viewaction.ViewGetter
 
 class MobileSwipe : RequestHandler<MobileSwipeParams, Void?> {
 
-    @Throws(AppiumException::class)
-    override fun handleInternal(params: MobileSwipeParams): Void? {
+    override fun handleEspresso(params: MobileSwipeParams): Void? {
         // Get a reference to the view and call onData. This will automatically scroll to the view.
         val viewInteraction = EspressoElement.getViewInteractionById(params.elementId)
 
         if (params.direction != null) {
             AndroidLogger.info("Performing swipe action with direction '${params.direction}'")
             when (params.direction) {
-                UP -> viewInteraction.perform(swipeUp())
-                DOWN -> viewInteraction.perform(swipeDown())
-                LEFT -> viewInteraction.perform(swipeLeft())
-                RIGHT -> viewInteraction.perform(swipeRight())
-                else -> throw InvalidArgumentException("Direction cannot be ${params.direction}")
+                UP -> viewInteraction.perform(androidx.test.espresso.action.ViewActions.swipeUp())
+                DOWN -> viewInteraction.perform(androidx.test.espresso.action.ViewActions.swipeDown())
+                LEFT -> viewInteraction.perform(androidx.test.espresso.action.ViewActions.swipeLeft())
+                RIGHT -> viewInteraction.perform(androidx.test.espresso.action.ViewActions.swipeRight())
+                else -> throw InvalidArgumentException(
+                    "Unknown swipe direction '${params.direction}'. " +
+                            "Only the following values are supported: " +
+                            values().joinToString(",") { x -> x.name.lowercase() }
+                )
             }
         } else if (params.swiper != null) {
-
             val runnable = object : UiControllerRunnable<Void?> {
                 override fun run(uiController: UiController): Void? {
                     val swipeAction = GeneralSwipeAction(
-                            params.swiper,
-                            params.startCoordinates,
-                            params.endCoordinates,
-                            params.precisionDescriber
+                        params.swiper,
+                        params.startCoordinates,
+                        params.endCoordinates,
+                        params.precisionDescriber
                     )
                     AndroidLogger.info("""
                     Performing general swipe action with parameters
                     swiper=[${params.swiper}] startCoordinates=[${params.startCoordinates}]
                     endCoordinates=[${params.endCoordinates}] precisionDescriber=[${params.precisionDescriber}]
-                    """.trimIndent())
+                    """.trimIndent()
+                    )
                     swipeAction.perform(uiController, ViewGetter().getView(viewInteraction))
                     return null
                 }
@@ -67,6 +75,24 @@ class MobileSwipe : RequestHandler<MobileSwipeParams, Void?> {
             UiControllerPerformer(runnable).run()
         }
 
+        return null
+    }
+
+    override fun handleCompose(params: MobileSwipeParams): Void? {
+        val nodeInteractions = getNodeInteractionById(params.elementId)
+
+        AndroidLogger.info("Performing swipe action with direction '${params.direction}'")
+        when (params.direction) {
+            UP -> nodeInteractions.performGesture { swipeUp() }
+            DOWN -> nodeInteractions.performGesture { swipeDown() }
+            LEFT -> nodeInteractions.performGesture { swipeLeft() }
+            RIGHT -> nodeInteractions.performGesture { swipeRight() }
+            else -> throw InvalidArgumentException(
+                "Unknown swipe direction '${params.direction}'. " +
+                        "Only the following values are supported: " +
+                        values().joinToString(",") { x -> x.name.lowercase() }
+            )
+        }
         return null
     }
 }
