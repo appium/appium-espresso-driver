bae6b7bb526bb3b5c36562bc8b7baf6c05af92f5
Mykola Mokhnach <mokhnach@gmail.com>
2025-01-31 19:43:02 +0100

    feat: Add support of executeMethodMap

--- test/unit/commands/execute-specs.js
+++ test/unit/commands/execute-specs.js
@@ -23,14 +23,14 @@ describe('commands', function () {
       sandbox.reset();
     });
     it('should raise error on non-existent mobile command', async function () {
-      await driver.executeMobile('fruta', {}).should.eventually.be.rejectedWith('Unknown mobile command "fruta"');
+      await driver.execute('mobile: fruta', {}).should.eventually.be.rejected;
     });
     it('should accept sensorSet on emulator', async function () {
       sandbox.stub(driver, 'isEmulator').returns(true);
       let stub = sandbox.stub(driver, 'sensorSet');
-      await driver.executeMobile('sensorSet', { sensorType: 'acceleration', value: '0:9.77631:0.812349' });
+      await driver.execute('mobile: sensorSet', [{ sensorType: 'acceleration', value: '0:9.77631:0.812349' }]);
       stub.calledOnce.should.equal(true);
-      stub.calledWithExactly({ sensorType: 'acceleration', value: '0:9.77631:0.812349' });
+      stub.calledWith('acceleration', '0:9.77631:0.812349');
     });
   });
 });
