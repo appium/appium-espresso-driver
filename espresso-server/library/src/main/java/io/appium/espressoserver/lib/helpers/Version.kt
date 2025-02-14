e69c7a6903e79f5f97298b001aa366c39c3dffb0
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-02-13 23:49:00 -0800

    Update Version.kt

--- espresso-server/library/src/main/java/io/appium/espressoserver/lib/helpers/Version.kt
+++ espresso-server/library/src/main/java/io/appium/espressoserver/lib/helpers/Version.kt
@@ -2,7 +2,6 @@ package io.appium.espressoserver.lib.helpers
 
 // This value is updated automatically by the NPM versioning script
 // It should be in sync with the NPM module version from package.json
-
 private const val VERSION = "4.0.1"
 
 fun getEspressoServerVersion() = VERSION
