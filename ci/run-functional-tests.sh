f370cd49ae678b6305c9caf15124637efbf0763f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-08-30 10:05:46 -0700

    modify script

--- ci/run-functional-tests.sh
+++ ci/run-functional-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 npx mocha --timeout 10m \
-    # ./test/functional/*-specs.js \
-    # ./test/functional/commands/attributes-e2e-specs.js \
     ./test/functional/commands/clipboard-e2e-specs.js \
     ./test/functional/commands/contexts-e2e-specs.js \
     ./test/functional/commands/element-values-e2e-specs.js \
-    # ./test/functional/commands/jetpack-compose-attributes-e2e-specs.js \
     -g @skip-ci -i --exit
+
+    # ./test/functional/*-specs.js \
+    # ./test/functional/commands/attributes-e2e-specs.js \
+    # ./test/functional/commands/jetpack-compose-attributes-e2e-specs.js \
