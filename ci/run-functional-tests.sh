d257eda46064761973fc78f0a0d9ea08e19ff864
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-09-01 13:00:07 -0700

    test: add one more for ci (#1085)

--- ci/run-functional-tests.sh
+++ ci/run-functional-tests.sh
@@ -11,6 +11,7 @@ npx mocha --timeout 10m \
     ./test/functional/commands/jetpack-compose-e2e-specs.js \
     ./test/functional/commands/keyboard-e2e-specs.js \
     ./test/functional/commands/attributes-e2e-specs.js \
+    ./test/functional/commands/orientation-e2e-specs.js \
     ./test/functional/commands/size-e2e-specs.js \
     ./test/functional/commands/source-e2e-specs.js \
     ./test/functional/*-specs.js \
