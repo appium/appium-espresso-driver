ae9ed21ad41cfdfc07f987fe08014281d068c119
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-09-01 22:19:40 -0700

    enable others

--- ci/run-functional-tests.sh
+++ ci/run-functional-tests.sh
@@ -2,4 +2,19 @@
 
 npx mocha --timeout 10m \
     ./test/functional/commands/mobile-e2e-specs.js \
+    ./test/functional/commands/jetpack-compose-source-e2e-specs.js \
+    ./test/functional/commands/element-values-e2e-specs.js \
+    ./test/functional/commands/contexts-e2e-specs.js \
+    ./test/functional/commands/element-values-e2e-specs.js \
+    ./test/functional/commands/find-e2e-specs.js \
+    ./test/functional/commands/jetpack-componse-element-values-e2e-specs.js \
+    ./test/functional/commands/jetpack-compose-attributes-e2e-specs.js \
+    ./test/functional/commands/jetpack-compose-e2e-specs.js \
+    ./test/functional/commands/keyboard-e2e-specs.js \
+    ./test/functional/commands/attributes-e2e-specs.js \
+    ./test/functional/commands/orientation-e2e-specs.js \
+    ./test/functional/commands/size-e2e-specs.js \
+    ./test/functional/commands/source-e2e-specs.js \
+    ./test/functional/commands/mobile-e2e-specs.js \
+    ./test/functional/*-specs.js \
     -g @skip-ci -i --exit
