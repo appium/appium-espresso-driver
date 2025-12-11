b153d04a8da5ac5a08d77fe17c748ff2e7db796f
Mykola Mokhnach <mokhnach@gmail.com>
2025-12-11 15:51:13 +0100

    fix script

--- ci/run-functional-tests.sh
+++ ci/run-functional-tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 npx mocha --timeout 10m \
-    ./test/functional/commands/jetpack-compose-source-e2e-specs.js \
-    ./test/functional/commands/element-values-e2e-specs.js \
-    ./test/functional/commands/contexts-e2e-specs.js \
-    ./test/functional/commands/element-values-e2e-specs.js \
-    ./test/functional/commands/find-e2e-specs.js \
-    ./test/functional/commands/jetpack-componse-element-values-e2e-specs.js \
-    ./test/functional/commands/jetpack-compose-attributes-e2e-specs.js \
-    ./test/functional/commands/jetpack-compose-e2e-specs.js \
-    ./test/functional/commands/keyboard-e2e-specs.js \
-    ./test/functional/commands/attributes-e2e-specs.js \
-    ./test/functional/commands/orientation-e2e-specs.js \
-    ./test/functional/commands/size-e2e-specs.js \
-    ./test/functional/commands/source-e2e-specs.js \
-    ./test/functional/commands/mobile-e2e-specs.js \
-    ./test/functional/webview/web-e2e-specs.js \
-    ./test/functional/webview/webatoms-e2e-specs.js \
-    ./test/functional/*-specs.js \
+    ./test/functional/commands/jetpack-compose-source-e2e-specs.ts \
+    ./test/functional/commands/element-values-e2e-specs.ts \
+    ./test/functional/commands/contexts-e2e-specs.ts \
+    ./test/functional/commands/element-values-e2e-specs.ts \
+    ./test/functional/commands/find-e2e-specs.ts \
+    ./test/functional/commands/jetpack-componse-element-values-e2e-specs.ts \
+    ./test/functional/commands/jetpack-compose-attributes-e2e-specs.ts \
+    ./test/functional/commands/jetpack-compose-e2e-specs.ts \
+    ./test/functional/commands/keyboard-e2e-specs.ts \
+    ./test/functional/commands/attributes-e2e-specs.ts \
+    ./test/functional/commands/orientation-e2e-specs.ts \
+    ./test/functional/commands/size-e2e-specs.ts \
+    ./test/functional/commands/source-e2e-specs.ts \
+    ./test/functional/commands/mobile-e2e-specs.ts \
+    ./test/functional/webview/web-e2e-specs.ts \
+    ./test/functional/webview/webatoms-e2e-specs.ts \
+    ./test/functional/*-specs.ts \
     -g @skip-ci -i --exit
