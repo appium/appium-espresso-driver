83edd51b1639a560a735b17518aa29c217e826bc
Mykola Mokhnach <mokhnach@gmail.com>
2026-01-17 10:01:13 +0100

    chore: Migrate the rest of commands to typescript (#1125)

--- lib/commands/actions.ts
+++ lib/commands/actions.ts
@@ -1,9 +1,16 @@
+import type { EspressoDriver } from '../driver';
+import type { StringRecord } from '@appium/types';
+
 /**
- * @this {import('../driver').EspressoDriver}
- * @param {import('@appium/types').StringRecord[]} actions
- * @returns {Promise<void>}
+ * Performs a sequence of W3C actions (e.g., pointer, key, wheel actions).
+ * Automatically converts pointer actions to touch type for mobile compatibility.
+ * @param actions - Array of action objects following the W3C Actions protocol
+ * @returns Promise that resolves when all actions are performed
  */
-export async function performActions(actions) {
+export async function performActions(
+  this: EspressoDriver,
+  actions: StringRecord[]
+): Promise<void> {
   this.log.debug(`Received the following W3C actions: ${JSON.stringify(actions, null, '  ')}`);
   // This is needed because Selenium API uses MOUSE as the default pointer type
   const preprocessedActions = actions.map((action) => ({
--- lib/commands/context.ts
+++ lib/commands/context.ts
@@ -1,10 +1,12 @@
+import type { EspressoDriver } from '../driver';
+import type { StringRecord } from '@appium/types';
+
 /**
  * Stop proxying to any Chromedriver and redirect to Espresso
  *
- * @this {import('../driver').EspressoDriver}
- * @returns {void}
+ * @returns void
  */
-export function suspendChromedriverProxy () {
+export function suspendChromedriverProxy (this: EspressoDriver): void {
   this.chromedriver = undefined;
   this.proxyReqRes = this.espresso.proxyReqRes.bind(this.espresso);
   this.proxyCommand = this.espresso.proxyCommand.bind(this.espresso);
@@ -24,17 +26,18 @@ export function suspendChromedriverProxy () {
  *     ...
  *   ]
  * }
- * @this {import('../driver').EspressoDriver}
  * @see https://github.com/appium/appium-espresso-driver?tab=readme-ov-file#mobile-webatoms
- * @param {string} webviewEl
- * @param {boolean} forceJavascriptEnabled
- * @param {import('@appium/types').StringRecord[]} methodChain
+ * @param webviewEl - Optional webview element ID to operate on
+ * @param forceJavascriptEnabled - If webview disables javascript, webatoms won't work, this forces it
+ * @param methodChain - Array of method chain objects, each containing a name and atom with name and args
+ * @returns Promise that resolves to the result of executing the web atoms
  */
 export async function mobileWebAtoms (
-  webviewEl,
-  forceJavascriptEnabled,
-  methodChain
-) {
+  this: EspressoDriver,
+  webviewEl?: string,
+  forceJavascriptEnabled?: boolean,
+  methodChain?: StringRecord[]
+): Promise<any> {
   return await this.espresso.jwproxy.command(`/appium/execute_mobile/web_atoms`, 'POST', {
     webviewEl,
     forceJavascriptEnabled,
