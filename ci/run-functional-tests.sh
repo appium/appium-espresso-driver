7bf060660ef6b04f63bc6fbd2a8d4a93e1ca2b92
Mykola Mokhnach <mokhnach@gmail.com>
2026-06-21 21:14:05 +0200

    fix script

--- ci/run-functional-tests.sh
+++ ci/run-functional-tests.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+set -e
 
-npx mocha --timeout 10m \
-    ./test/functional/commands/jetpack-compose-source-e2e-specs.ts \
-    ./test/functional/commands/element-values-e2e-specs.ts \
-    ./test/functional/commands/contexts-e2e-specs.ts \
-    ./test/functional/commands/element-values-e2e-specs.ts \
-    ./test/functional/commands/find-e2e-specs.ts \
-    ./test/functional/commands/jetpack-componse-element-values-e2e-specs.ts \
-    ./test/functional/commands/jetpack-compose-attributes-e2e-specs.ts \
-    ./test/functional/commands/jetpack-compose-e2e-specs.ts \
-    ./test/functional/commands/keyboard-e2e-specs.ts \
-    ./test/functional/commands/attributes-e2e-specs.ts \
-    ./test/functional/commands/orientation-e2e-specs.ts \
-    ./test/functional/commands/size-e2e-specs.ts \
-    ./test/functional/commands/source-e2e-specs.ts \
-    ./test/functional/commands/mobile-e2e-specs.ts \
-    ./test/functional/webview/web-e2e-specs.ts \
-    ./test/functional/webview/webatoms-e2e-specs.ts \
-    ./test/functional/*-specs.ts \
-    -g @skip-ci -i --exit
+npm run pree2e-test
+
+node --test --test-concurrency=1 --test-timeout=600000 --test-skip-pattern @skip-ci \
+    ./build/test/functional/commands/jetpack-compose-source-e2e-specs.js \
+    ./build/test/functional/commands/element-values-e2e-specs.js \
+    ./build/test/functional/commands/contexts-e2e-specs.js \
+    ./build/test/functional/commands/find-e2e-specs.js \
+    ./build/test/functional/commands/jetpack-componse-element-values-e2e-specs.js \
+    ./build/test/functional/commands/jetpack-compose-attributes-e2e-specs.js \
+    ./build/test/functional/commands/jetpack-compose-e2e-specs.js \
+    ./build/test/functional/commands/keyboard-e2e-specs.js \
+    ./build/test/functional/commands/attributes-e2e-specs.js \
+    ./build/test/functional/commands/orientation-e2e-specs.js \
+    ./build/test/functional/commands/size-e2e-specs.js \
+    ./build/test/functional/commands/source-e2e-specs.js \
+    ./build/test/functional/commands/mobile-e2e-specs.js \
+    ./build/test/functional/webview/web-e2e-specs.js \
+    ./build/test/functional/webview/webatoms-e2e-specs.js \
+    ./build/test/functional/*-specs.js
